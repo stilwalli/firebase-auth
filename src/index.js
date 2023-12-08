d0e1dd484ea1b7a44e5a8b8786a3ed2f15821599
Shashank Tilwalli <stilwalli@google.com>
2023-12-08 09:24:16 -0600

    auth

---
+++ src/index.js
@@ -0,0 +1,96 @@
+import './styles.css';
+import { 
+  hideLoginError, 
+  showLoginState, 
+  showLoginForm, 
+  showApp, 
+  showLoginError, 
+  btnLogin,
+  btnSignup,
+  btnLogout
+} from './ui'
+
+import { initializeApp } from 'firebase/app';
+import { 
+  getAuth,
+  onAuthStateChanged, 
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  connectAuthEmulator
+} from 'firebase/auth';
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAcOI1ZZ6MOvlwsjXIdOGnX3PCHlrKF5L4",
+  authDomain: "genai-387917.firebaseapp.com",
+  projectId: "genai-387917",
+  storageBucket: "genai-387917.appspot.com",
+  messagingSenderId: "95065625117",
+  appId: "1:95065625117:web:674bdeff6e1afd8026ecf4",
+  measurementId: "G-QBR7MLRRCH"
+});
+
+// Login using email/password
+const loginEmailPassword = async () => {
+  const loginEmail = txtEmail.value
+  const loginPassword = txtPassword.value
+
+  // step 1: try doing this w/o error handling, and then add try/catch
+  //await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+
+  // step 2: add error handling
+   try {
+     await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+   }
+   catch(error) {
+     console.log(`There was an error: ${error}`)
+     showLoginError(error)
+   }
+}
+
+// Create new account using email/password
+const createAccount = async () => {
+  const email = txtEmail.value
+  const password = txtPassword.value
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password)
+  }
+  catch(error) {
+    console.log(`There was an error: ${error}`)
+    showLoginError(error)
+  } 
+}
+
+// Monitor auth state
+const monitorAuthState = async () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user)
+      showApp()
+      showLoginState(user)
+
+      hideLoginError()
+      hideLinkError()
+    }
+    else {
+      showLoginForm()
+      lblAuthState.innerHTML = `You're not logged in.`
+    }
+  })
+}
+
+// Log out
+const logout = async () => {
+  await signOut(auth);
+}
+
+btnLogin.addEventListener("click", loginEmailPassword) 
+//btnSignup.addEventListener("click", createAccount)
+btnLogout.addEventListener("click", logout)
+
+const auth = getAuth(firebaseApp);
+
+//connectAuthEmulator(auth, "http://localhost:9099");
+
+monitorAuthState();
